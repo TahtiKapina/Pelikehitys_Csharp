c91e84f2d8616f722cae3173b7ec783481490878
Daniel <s2401272@edu.bc.fi>
2026-01-29 13:33:44 +0200

    metodit keksen
    Methodit eivät ole viellä valmiina ja on erroreita

--- Pelikehitys_Csharp/C# perusteet kertaus/Program.cs
+++ Pelikehitys_Csharp/C# perusteet kertaus/Program.cs
@@ -2,6 +2,13 @@
 {
     internal class Program
     {
+        static int Vahinkopisteet(int min, int max)
+        {
+            Random random = new Random();
+
+            return random.Next(min, max);
+        }
+
         static void Main(string[] args)
         {
 
@@ -14,53 +21,15 @@
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("Löydät örkin metsästä ja tämä hyökkää sinua kohti. Taistelu alkaa");
 
-            while (Ritari > 0 || Örkki > 0)
+
+            while (Ritari > 0 && Örkki > 0)
             {
-
-                Console.WriteLine("------------------------------------------------------------------");
-
-                Console.WriteLine($"Ritari(Sinä): {Ritari}/15   Örkki: {Örkki}/15");
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("1 - Hyökkää miekalla");
-                Console.WriteLine("2 - Puolustaudu kilvellä");
-
-                Console.Write("Mitä haluat tehdä? ");
-                aikomus = Convert.ToInt32(Console.ReadLine());
-
-                static int Vahinkopisteet(int min, int max)
-                {
-                    Random random = new Random();
-
-                    return random.Next(min, max);
-                }
+                GameState(Ritari, Örkki, aikomus);
 
                 int Pelaajavahinko = Vahinkopisteet(1, 7);
                 int Örkkivahinko = Vahinkopisteet(1, 5);
 
-                if (aikomus == 1)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Hyökäsit miekalasi!");
-                    Console.WriteLine($"Sivallat örkkiä miekallasi. Teet örkkiin {Pelaajavahinko} pistettä vahinkoa");
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Örkki hyökkää sinua kohti nuijallaan!");
-                    Console.WriteLine($"Nuija osuu sinuun, tehden {Örkkivahinko} vahinkoa.");
-
-                    Ritari -= Örkkivahinko;
-                    Örkki -= Pelaajavahinko;
-                }
-                else
-                {
-                    int VähenettyVahinko = Örkkivahinko / 2;
-
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Nostat kilpesi puolustukseksesi!");
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Örkki hyökkää sinua kohti nuijallaan!");
-                    Console.WriteLine($"Nuija kumahtaa kilpeesi, tehden sinuun vain {VähenettyVahinko} vahinkoa");
-
-                    Ritari -= VähenettyVahinko;
-                }
+                PlayerAction(ref Ritari, ref Örkki, aikomus, Pelaajavahinko, Örkkivahinko);
 
                 if (Örkki < 1)
                 {
@@ -98,6 +67,47 @@
                 Console.WriteLine("Örkki onnistui päihitämään ritarin");
             }
             Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        static void GameState(int Ritari, int Örkki, int aikomus)
+        {
+            Console.WriteLine("------------------------------------------------------------------");
+
+            Console.WriteLine($"Ritari(Sinä): {Ritari}/15   Örkki: {Örkki}/15");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("1 - Hyökkää miekalla");
+            Console.WriteLine("2 - Puolustaudu kilvellä");
+
+            Console.Write("Mitä haluat tehdä? ");
+            aikomus = Convert.ToInt32(Console.ReadLine());
+        }
+
+        static void PlayerAction(ref int Ritari, ref int Örkki, int Örkkivahinko, int aikomus, int Pelaajavahinko)
+        {
+            if (aikomus == 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Hyökäsit miekalasi!");
+                Console.WriteLine($"Sivallat örkkiä miekallasi. Teet örkkiin {Pelaajavahinko} pistettä vahinkoa");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Örkki hyökkää sinua kohti nuijallaan!");
+                Console.WriteLine($"Nuija osuu sinuun, tehden {Örkkivahinko} vahinkoa.");
+
+                Ritari -= Örkkivahinko;
+                Örkki -= Pelaajavahinko;
+            }
+            else
+            {
+                int VähenettyVahinko = Örkkivahinko / 2;
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Nostat kilpesi puolustukseksesi!");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Örkki hyökkää sinua kohti nuijallaan!");
+                Console.WriteLine($"Nuija kumahtaa kilpeesi, tehden sinuun vain {VähenettyVahinko} vahinkoa");
+
+                Ritari -= VähenettyVahinko;
+            }
         }
     }
 }
