5f84df479062c755a3b6c66e82663c80c42b705d
Daniel <s2401272@edu.bc.fi>
2026-02-23 10:43:57 +0200

    Nuoli Kauppa scripti aloitettu
    Nuoli Kauppa scripti aloitettu ja luokka luotu

---
+++ Nuoli Kauppa/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Nuoli_Kauppa
+{
+    internal class Program
+    {
+        enum Kärki   
+        {
+            Puu,
+            Teräs,
+            Timantti
+        }
+
+        enum Sulat
+        {
+            Lehti,
+            kanansulka,
+            kotkansulka
+        }
+
+        class Nuoli
+        {
+            public Kärki Kärki { get; set; }
+            public Sulat Sulat { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
