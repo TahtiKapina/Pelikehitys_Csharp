31b8951444d9e61a54be6495c5c2c0eb0750dbe2
Daniel <s2401272@edu.bc.fi>
2026-03-20 12:29:34 +0200

    Nuoli kauppa päivitetty. Tehtävä 3.4 tehty
    tehtävä 3.4 on tehty

--- Nuoli Kauppa/Program.cs
+++ Nuoli Kauppa/Program.cs
@@ -23,6 +23,21 @@
             public Kärki kärki { get; private set; }
             public Sulat sulat { get; private set; }
 
+            public static Nuoli EliittiNuoli()
+            {
+                return new Nuoli(Kärki.Timantti, Sulat.kotkansulka) { Pituus = 100 };
+            }
+
+            public static Nuoli Aloitelijanuoli()
+            {
+                return new Nuoli(Kärki.Puu, Sulat.kanansulka) { Pituus = 70 };
+            }
+
+            public static Nuoli Perusnuoli()
+            {
+                return new Nuoli(Kärki.Teräs, Sulat.kanansulka) { Pituus = 85 };
+            }
+
             public int Pituus
             {
                 get { return pituus; }
@@ -49,61 +64,106 @@
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Minkälainen kärki (puu, teräs, timantti)?: ");
-            string KärkiInput = Console.ReadLine();
-            Nuoli n = new Nuoli(Enum.Parse<Kärki>(KärkiInput, true), Sulat.Lehti);
-            Console.WriteLine("Minkälaiset sulat (lehti, kanansulka, kotkansulka)?:");
-            string SulatInput = Console.ReadLine();
+            Console.WriteLine("Tervetuloa nuolikauppaan.");
+            Console.WriteLine("Haluatko: ");
+            Console.WriteLine("1. Teettää nuolen tilaustyönä?");
+            Console.WriteLine("2. Ostaa valmiin nuolen?");
+            Console.WriteLine("Valinta: ");
+            Console.WriteLine("");
+            string Valinta = Console.ReadLine();
 
-            Console.WriteLine("Nuolen pituus sentteinä (60-100): ");
-            int pituus = Convert.ToInt32(Console.ReadLine());
-
-            Kärki kärki = Enum.Parse<Kärki>(KärkiInput, true);
-            Sulat sulat = Enum.Parse<Sulat>(SulatInput, true);
-
-            float Summa = KärkiHinta(kärki) + SulkienHinta(sulat) + PituusHinta(pituus);
-
-
-            Console.WriteLine($"Tämän nuolen hinta on {Summa} kultarahaa");
-        }
-
-        static int KärkiHinta(Kärki kärki)
-        {
-            switch (kärki)
+            if (Valinta == "1")
             {
-                case Kärki.Puu:
-                    return 3;
-                case Kärki.Teräs:
-                    return 5;
-                case Kärki.Timantti:
-                    return 50;
-                default:
-                    return 0;
-            }
-        }
+                Console.WriteLine("Minkälainen kärki (puu, teräs, timantti)?: ");
+                string KärkiInput = Console.ReadLine();
+                Nuoli n = new Nuoli(Enum.Parse<Kärki>(KärkiInput, true), Sulat.Lehti);
+                Console.WriteLine("Minkälaiset sulat (lehti, kanansulka, kotkansulka)?:");
+                string SulatInput = Console.ReadLine();
 
-        static int SulkienHinta(Sulat sulat)
-        {
-            switch (sulat)
-            {
-                case Sulat.Lehti:
-                    return 0;
-                case Sulat.kanansulka:
-                    return 1;
-                case Sulat.kotkansulka:
-                    return 5;
-                default:
-                    return 0;
-            }
-        }
+                Console.WriteLine("Nuolen pituus sentteinä (60-100): ");
+                int pituus = Convert.ToInt32(Console.ReadLine());
 
-        static float PituusHinta(int pituus)
-        {
-            if (pituus < 60 || pituus > 100)
-            {
-                throw new ArgumentOutOfRangeException("Pituuden tulee olla välillä 60-100 senttimetriä.");
+                Kärki kärki = Enum.Parse<Kärki>(KärkiInput, true);
+                Sulat sulat = Enum.Parse<Sulat>(SulatInput, true);
+
+                float Summa = KärkiHinta(kärki) + SulkienHinta(sulat) + PituusHinta(pituus);
+
+                Console.WriteLine($"Tämän nuolen hinta on {Summa} kultarahaa");
             }
-            return pituus * 0.05f;
+
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Valitse valmis nuoli: ");
+                Console.WriteLine("1. Eliittinuoli");
+                Console.WriteLine("2. Aloittelijaanuoli");
+                Console.WriteLine("3. Perusnuoli");
+                string NuoliValinta = Console.ReadLine();
+
+                if (NuoliValinta == "1")
+                {
+                    Nuoli n = Nuoli.EliittiNuoli();
+                    float Summa = KärkiHinta(n.GetKärki()) + SulkienHinta(n.GetSulat()) + PituusHinta(n.Pituus);
+                    Console.WriteLine($"Tämän nuolen hinta on {Summa} kultarahaa");
+                }
+                else if (NuoliValinta == "2")
+                {
+                    Nuoli n = Nuoli.Aloitelijanuoli();
+                    float Summa = KärkiHinta(n.GetKärki()) + SulkienHinta(n.GetSulat()) + PituusHinta(n.Pituus);
+                    Console.WriteLine($"Tämän nuolen hinta on {Summa} kultarahaa");
+                }
+                else if (NuoliValinta == "3")
+                {
+                    Nuoli n = Nuoli.Perusnuoli();
+                    float Summa = KärkiHinta(n.GetKärki()) + SulkienHinta(n.GetSulat()) + PituusHinta(n.Pituus);
+                    Console.WriteLine($"Tämän nuolen hinta on {Summa} kultarahaa");
+                }
+                else
+                {
+                    Console.WriteLine("Virheellinen valinta.");
+                }
+
+
+            }
+
+            static int KärkiHinta(Kärki kärki)
+            {
+                switch (kärki)
+                {
+                    case Kärki.Puu:
+                        return 3;
+                    case Kärki.Teräs:
+                        return 5;
+                    case Kärki.Timantti:
+                        return 50;
+                    default:
+                        return 0;
+                }
+            }
+
+            static int SulkienHinta(Sulat sulat)
+            {
+                switch (sulat)
+                {
+                    case Sulat.Lehti:
+                        return 0;
+                    case Sulat.kanansulka:
+                        return 1;
+                    case Sulat.kotkansulka:
+                        return 5;
+                    default:
+                        return 0;
+                }
+            }
+
+            static float PituusHinta(int pituus)
+            {
+                if (pituus < 60 || pituus > 100)
+                {
+                    throw new ArgumentOutOfRangeException("Pituuden tulee olla välillä 60-100 senttimetriä.");
+                }
+                return pituus * 0.05f;
+            }
         }
     }
 }
